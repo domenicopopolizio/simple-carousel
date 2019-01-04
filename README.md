5d06065971ead1b03e7533c1b79a61658335ccf2
domenico <treedevelopers@protonmail.com>
2019-01-04 22:27:07 +0100

    deleted useless scroll-behavior smooth in css
    added simple readme
    
    code commented

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # simple-carousel
 simple carousel
+It works only with webkit (otherwise it will work but a scrollbar will appear under each carousel)
+
+It's not a library, just a code for me that I'll reuse in future, but it could be useful for anyone if they understand how does it work.
--- simple-carousel.js
+++ simple-carousel.js
@@ -1,33 +1,61 @@
-const mod = (i, N) => ( i % N + N ) % N;
-
-
+// carousel class
 class Carousel {
-    constructor(elem, listOfImageSrc = []) {
+    // it needs the element that should became the carousel itself
+    constructor(elem) {
+
+        // the element is saved
         this.element = elem;
-        this.listOfImageSrc = listOfImageSrc;
+        // list of elements inside the carousel
         this.content = [...elem.children];
-        let length = Math.min(this.content.length, this.listOfImageSrc.length);
-        for(let i = 0; i < length; i++) {
-            this.content[i].style.backgroundImage = `url(${listOfImageSrc[i]})`;
-        }
-        for(let el of this.content) {
+
+        // each one should have an event to select it
+        for (let el of this.content) {
             el.addEventListener("click", this.select);
         }
-        this.select({target:this.content[0]});
+
+        // the first one should be selected, so here the event is triggered on the first
+        // element of the current carousel
+        this.select({
+            target: this.content[0]
+        });
     }
+
+    // current carousel
     select(ev) {
-        ev.target.parentNode.scrollTo({top: 0, left: ev.target.offsetLeft - window.innerWidth/2 + ev.target.clientWidth/2, behavior: "smooth"});
+        // it works on ev.target and not on this.element because when an event is called, "this" refers to
+        // the caller, not to the current istance of the object
+
+        // it scrolls the parent of the clicked element (it scrolls the carousel)
+        // in such a way to center the clicked element on the carousel 
+        // smoothly
+        ev.target.parentNode.scrollTo({
+            top: 0,
+            left: ev.target.offsetLeft - window.innerWidth / 2 + ev.target.clientWidth / 2,
+            behavior: "smooth"
+        });
+
+        // the current selected element of the carousel is not more the current selected element
+        // (It iterate through all the .current element, but it should be just one element)
         ev.target.parentNode.querySelectorAll(".current").forEach(curr => {
             curr.classList.remove("current");
         });
+
+        //and the new current element is the one the user have clicked on 
         ev.target.classList.add("current");
 
     }
 }
 
+
+// just for the example
 document.addEventListener("DOMContentLoaded", () => {
+    
+    // let els be the group of div that should became a carousel
     let els = document.querySelectorAll(".sc-container");
-    for(let el of els) {
+
+    // for each of this div
+    for (let el of els) {
+        // a carousel istance is created 
         new Carousel(el);
     }
 });
